8ef85284af29fdd68ac7be5a9be18dd5c5827a70
Andres Jerriel Sinabutar <andres.sinabutar@gmail.com>
2021-03-19 11:20:35 +0700

    Create Tile class

---
+++ src/Tile/Tile.cpp
@@ -0,0 +1,40 @@
+#include "Tile.h"
+
+Tile::Tile(string _type) {
+    type = _type;
+    isEngimonOccupied = false;
+    wild_engimon = NULL;
+}
+
+Tile::Tile(string _type, Engimon* _wild_engimon) {
+    type =_type;
+    wild_engimon = _wild_engimon;
+}
+
+Tile::~Tile() {
+    delete wild_engimon;
+}
+
+string Tile::getType() {
+    return type;
+}
+
+bool Tile::getIsEngimonOccupied() {
+    return isEngimonOccupied;
+}
+
+Engimon Tile::getWildEngimon() {
+    return *wild_engimon;
+}
+
+void Tile::setType(string _type) {
+    type = _type;
+}
+
+void Tile::setIsEngimonOccupied(bool _isEngimonOccupied) {
+    isEngimonOccupied = _isEngimonOccupied;
+}
+
+void Tile::setWildEngimon(Engimon* _wild_engimon) {
+    wild_engimon = _wild_engimon;
+}
---
+++ src/Tile/Tile.h
@@ -0,0 +1,40 @@
+#ifndef _TILE_H_
+#define _TILE_H_
+
+#define NULL -1
+
+#include "../Engimon.h"
+#include <string>
+using namespace std;
+
+class Tile {
+    private:
+        string type;
+        bool isEngimonOccupied;
+        Engimon* wild_engimon;
+    
+    public:
+        // user-defined ctor for non engimon-occupied tiles
+        Tile(string _type);
+
+        // user-defined ctor for engimon-occupied tiles
+        Tile(string _type, Engimon* _wild_engimon);
+
+        // cctor
+        Tile(const Tile&);
+
+        // dtor
+        ~Tile();
+
+        // getter
+        string getType();
+        bool getIsEngimonOccupied();
+        Engimon getWildEngimon();
+
+        // setter
+        void setType(string _type);
+        void setIsEngimonOccupied(bool _isEngimonOccupied);
+        void setWildEngimon(Engimon* _wild_engimon);
+};
+
+#endif _TILE_H_
