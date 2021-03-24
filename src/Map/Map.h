0718b9b5f9e6e43442d9caf4716ac184ee9f52fd
Gayuh Tri Rahutami <69070763+wundersmith@users.noreply.github.com>
2021-03-24 18:34:01 +0700

    mindahin tile

--- src/Map/Map.h
+++ src/Map/Map.h
@@ -1,7 +1,7 @@
 #ifndef MAP_H
 #define MAP_H
 // #include "../Tile/Tile.h"
-#include "../Tile/Tile.h"
+#include "Tile.h"
 #include "Position.h"
 #include "../Player/Player.hpp"
 #include "../Engimon/EngimonFactory.h"
--- src/Map/compilemap.bat
+++ src/Map/compilemap.bat
@@ -1,1 +1,1 @@
-g++ drivermap.cpp Map.cpp ../Tile/Tile.cpp Position.cpp ../Engimon/Engimon.cpp ../Engimon/Engimons.cpp ../Engimon/EngimonFactory.cpp ../Skill/Skill.cpp -o map
+g++ drivermap.cpp Map.cpp Tile.cpp Position.cpp ../Engimon/Engimon.cpp ../Engimon/Engimons.cpp ../Engimon/EngimonFactory.cpp ../Skill/Skill.cpp -o map
